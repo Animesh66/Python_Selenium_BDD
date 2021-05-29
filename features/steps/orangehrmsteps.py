cc072f114dfa69b043a1ff333de6bccaba2f3cd5
Animesh <er.animesh6@hotmail.com>
2021-05-29 17:48:07 +0530

    Step def added

--- features/steps/orangehrmsteps.py
+++ features/steps/orangehrmsteps.py
@@ -4,20 +4,24 @@ from selenium import webdriver
 
 @given('launch Chrome browser')
 def step_launch_browser(context):
-    raise NotImplementedError(u'STEP: Given launch Chrome browser')
+    context.driver = webdriver.Chrome()
+    context.driver.maximize_window()
 
 
 @when('open orangeHRM homepage')
 def step_open_homepage(context):
-    raise NotImplementedError(u'STEP: When open orangeHRM homepage')
+    context.driver.get("https://opensource-demo.orangehrmlive.com/")
+    context.driver.set_page_load_timeout(10)
 
 
 @then('Verify that the logo present on the page')
 def step_verify_logo(context):
-    raise NotImplementedError(u'STEP: Then Verify that the logo present on the page')
+    logo_status = context.driver.find_element_by_xpath("//*[@id='divLogo']/img").is_displayed()
+    assert logo_status is True  # this is assertion method
 
 
 @then('close the browser')
 def step_close_browser(context):
-    raise NotImplementedError(u'STEP: Then close the browser')
+    context.driver.close()
+
 
